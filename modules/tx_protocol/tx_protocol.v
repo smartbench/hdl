36cc472603b886cc35f5b11b23a304beb19b165b
ivan <ivanpauno@gmail.com>
2017-08-02 22:01:08 -0300

    Restarted module. First input intersface to have data ready is bypassed to ft245 tx interface. End of transmission is with eof interface signal.

--- modules/tx_protocol/tx_protocol.v
+++ modules/tx_protocol/tx_protocol.v
@@ -1,13 +1,6 @@
 /*
     Tx protocol module.
-    This module sends sampled data, or internal status (trigger, etc).
-    Computer is master in this communication, data is sended from the FPGA responding to computer's requestes.
-    Priority:
-            - Send internal status.
-            - Send sampled data.
-            - status bits ??
-    Frames start with a heather, indicating if the data sended is status, registes, samples.
-    Frames are fixed length, defined by FRAME_LENGTH parameter.
+    This module sends sampled data, or trigger status.
 
     Authors:
                 AD      Andres Demski
@@ -17,41 +10,118 @@
     Version:
                 Date           Modified by         Comment
                 2017/07/31     IP                  Module created. Only IO signals writen. Behavioural haven't been done.
+    ToDo:
+                Date           Suggested by        Comment
+                2017/08/02     IP                  Package all input interfaces in a flattened 2D array ??
 */
 
+/* WAITING FOR DEFINES:
+    TRIGGER STATUS requested
+    SAMPLED DATA requested */
+
 module tx_protocol #(
-    parameter FRAME_LENGTH = 20,
-    parameter TX_WIDTH = 8,
-    parameter ADC_WIDTH = 8
+    parameter DATA_WIDTH = 8,
+    parameter TX_WIDTH = 8
 ) (
-    // Checks if status, registers, data was requested.
-    input request_register,
+    // Basic synchronous signals
+    input clk,
+    input rst,
 
-    // Sampled data buffer interface (interface with circular buffer)
-    input buffer_rdy,                   // Valid data indication
-    input [ADC_WIDTH-1:0] buffer_data,  // Data
-    output buffer_reading,              // Set this bit when buffer is requested.
-    output buffer_ack,                  // Current data has been read. Requesting next sample.
+    // Output interface
+    output reg [TX_WIDTH-1:0] tx_data,
+    output reg tx_rdy,
+    output reg tx_ack,
 
-    // Buffer status (interface with buffer controller)
-    input buffer_full,                  // Buffer rdy can be used instead ??
-    output buffer_reading_ended,        // falling edge of buffer_reading could be used instead.
+    // SI interface for channel 1
+    input [DATA_WIDTH-1:0] ch1_data,    // Data bus
+    input ch1_rdy,                      // Valid data in the bus
+    output ch1_ack,                     // Ack
+    input ch1_eof,                      // No more channel 1 data avalaible
 
-    //
-    input shift_reg_out,                // Registers data
-    input shift_reg_empty,              // No more data to read
-    output shift_reg_ack                // Data has been read
+    // SI interface for channel 2
+    input [DATA_WIDTH-1:0] ch2_data,    // Data bus
+    input ch2_rdy,                      // Valid data in the bus
+    output ch2_ack,                     // Ack
+    input ch2_eof,                      // No more channel 1 data avalaible
 
+    // SI interface for trigger status
+    input [DATA_WIDTH-1:0] tr_data,     // Data bus
+    input tr_rdy,                       // Valid data in the bus
+    output tr_ack,                      // Ack
+    input tr_eof                        // No more channel 1 data avalaible
 );
 
-/* Header format:
-    xxxxxxyy
-    y:  00, Header only (+status ??)
-        01, sending registers.
-        10, sendig data.
-        11, future feature.
-    xxxxxx: status bits indication (trigger, etc)
-    */
-    parameter request_status_bit;
+    parameter MAX_COUNT =  $rtoi($ceil($itor(DATA_WIDTH)/TX_WIDTH)) - 1;
+    parameter COUNTER_BITS = (MAX_COUNT == 0) ? 1 : $clog2( MAX_COUNT );
 
+    reg [COUNTER_BITS-1:0] cnt;
+
+    // The state machine have to simple status.
+    // It's in iddle state when it's no data to send.
+    // When one of the three input interfaces are ready, state changes to sending and source interface is stored.
+    // When this interface indicates eof, status changes to iddle.
+
+    parameter ST_IDDLE = 0;
+    parameter ST_SENDING = 1;
+
+    reg state;
+
+    reg [1:0] source_interface;
+
+    // 2D array packing input interfaces.
+    reg [DATA_WIDTH-1:0] input_data [0:2];
+    reg [0:2] input_rdy;
+    reg [0:2] input_ack;
+    reg [0:2] input_eof;
+
+
+    always @* begin
+        input_data[0] <= tr_data;
+        input_data[1] <= ch1_data;
+        input_data[2] <= ch2_data;
+
+        input_rdy[0] <= tr_rdy;
+        input_rdy[1] <= ch1_rdy;
+        input_rdy[2] <= ch2_rdy;
+
+        input_ack[0] <= tr_ack;
+        input_ack[1] <= ch1_ack;
+        input_ack[2] <= ch2_ack;
+
+        input_eof[0] <= tr_eof;
+        input_eof[1] <= ch1_eof;
+        input_eof[2] <= ch2_eof;
+    end
+
+    always @( posedge(clk) ) begin
+        if ( rst ) begin
+            cnt <= MAX_COUNT;
+            state <= ST_IDDLE;
+        end else begin
+            if( ( state == ST_IDDLE ) && ( |input_ack == 1'b1 ) ) begin
+                state <= ST_SENDING;
+
+                // A better way to write this ???
+                if( input_ack[0] == 1'b1 ) begin
+                    source_interface <= 0;
+                end else begin
+                    if( input_ack[1] == 1'b1 ) begin
+                        source_interface <= 1;
+                    end else begin
+                        source_interface <= 2;
+                    end
+                end
+            end
+
+            if( ( state == ST_SENDING ) ) begin
+                if( input_eof != 1'b1 ) begin
+                    tx_data <= input_data[source_interface];
+                    tx_rdy <= input_rdy[source_interface];
+                    tx_ack <= input_ack[source_interface];
+                end else begin
+                    state <= ST_IDDLE;
+                end
+            end
+        end
+    end
 endmodule
