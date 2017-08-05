a347099de8561d14c868f2ebe8778aee732f395a
kuku22 <kuku2206@gmail.com>
2017-08-05 18:15:35 -0300

    added testbench. Working!

--- modules/tx_protocol/test/tx_protocol.py
+++ modules/tx_protocol/test/tx_protocol.py
@@ -17,34 +17,48 @@ def nsTimer (t):
 
 
 class SOURCE:
-    def __init__ (self, clk, data, rqst, eof, ack):
+    def __init__ (self, clk, data, rdy, eof, ack):
         self.clk = clk
         self.data = data
-        self.rqst = rqst
+        self.rdy = rdy
         self.eof = eof
         self.ack = ack
-        self.fifo = []
 
+        self.fifo = []
         self.data <= 0
-        self.eof <= 0
+        self.rdy <= 0
+        self.eof <= 1
 
     def write(self,val):
         self.fifo.append(val)
 
     @cocotb.coroutine
+    def raise_rdy(self):
+        if(len(self.fifo) > 0):
+            self.data <= self.fifo.pop(0)
+            self.rdy <= 1
+            self.eof <= 0
+        else:
+            self.rdy <= 0
+            self.eof <= 1
+        yield RisingEdge(self.clk)
+
+
+    @cocotb.coroutine
     def data_source (self):
         while True:
-            while(self.rqst == 0): yield RisingEdge(self.clk)
+            if(self.ack.value.integer == 1):
+                if(len(self.fifo) > 0):
+                    self.data <= self.fifo.pop(0)
+                else:
+                    self.eof <= 1
+                    self.rdy <= 0
             yield RisingEdge(self.clk)
-            while (len(self.fifo) > 0):
-                self.data <= self.fifo.pop(0)
-                yield RisingEdge(self.clk)
-                while(self.ack.value.integer == 0): yield RisingEdge(self.clk)
-            self.eof <= 1
-            yield RisingEdge(self.clk)
-            self.eof <= 0
-            yield RisingEdge(self.clk)
-
+            # add later: random delay between 0 and 2 clocks
+            #i = randint(0,2)
+            #while(i > 0):
+            #    yield RisingEdge(self.clk)
+            #    i = i - 1
 
 class FT245:
     def __init__ (self, clk, tx_data, tx_rdy, tx_ack):
@@ -59,17 +73,18 @@ class FT245:
     @cocotb.coroutine
     def tx_driver (self):
         while True:
-            while(self.tx_rdy == 0):
-                yield RisingEdge(self.clk)
-            i = randint(0,2)
-            # random delay between 0 and 2 clocks
-            while(i > 0):
-                yield RisingEdge(self.clk)
-                i = i - 1
-            self.fifo.append(self.tx_data.value.integer)
-            self.tx_ack <= 1
-            yield RisingEdge(self.clk)
+            yield Timer(1,'ps')
             self.tx_ack <= 0
+            if(self.tx_rdy.value.integer == 1):
+                self.tx_ack <= 1
+                self.fifo.append(self.tx_data.value.integer)
+            yield RisingEdge(self.clk)
+
+            # add later: random delay between 0 and 2 clocks
+            #i = randint(0,2)
+            #while(i > 0):
+            #    yield RisingEdge(self.clk)
+            #    i = i - 1
 
 @cocotb.coroutine
 def Reset (dut):
@@ -83,16 +98,14 @@ def Reset (dut):
 def test (dut):
     fifo_test_ch1 = []
     fifo_test_ch2 = []
-    samples_ch1 = 100
+    samples_ch1 = 30
     samples_ch2 = 24
 
-    dut.rqst_rdy <= 0
-    dut.rqst_id <= 0
-    dut.rst <= 1
+    #dut.rst <= 1
 
-    src_ch1 = SOURCE(dut.clk, dut.ch1_data, dut.ch1_rqst, dut.ch1_eof, dut.ack)
-    src_ch2 = SOURCE(dut.clk, dut.ch2_data, dut.ch2_rqst, dut.ch2_eof, dut.ack)
-    src_trig = SOURCE(dut.clk, dut.trig_data, dut.trig_rqst, dut.trig_eof, dut.ack)
+    src_ch1 = SOURCE(dut.clk, dut.ch1_data, dut.ch1_rdy, dut.ch1_eof, dut.ch1_ack)
+    src_ch2 = SOURCE(dut.clk, dut.ch2_data, dut.ch2_rdy, dut.ch2_eof, dut.ch2_ack)
+    src_trig = SOURCE(dut.clk, dut.trig_data, dut.trig_rdy, dut.trig_eof, dut.trig_ack)
 
     ft245 = FT245(dut.clk, dut.tx_data, dut.tx_rdy, dut.tx_ack)
 
@@ -120,53 +133,52 @@ def test (dut):
     cocotb.fork(src_ch1.data_source() )
     cocotb.fork(src_ch2.data_source() )
 
-    yield RisingEdge(dut.clk)
+    for i in range(10): yield RisingEdge(dut.clk)
+    yield src_ch1.raise_rdy()
+    #src_ch1.raise_rdy()
+    for i in range(20): yield RisingEdge(dut.clk)
+    yield src_ch2.raise_rdy()
+
+
+    # Now wait for the data to be sent
+    for i in range(50): yield RisingEdge(dut.clk)
 
     # Start of the test
     err = 0
-
-    # Testing source: ch1
-    ft245.fifo = []
-    dut.rqst_id <= 1
-    dut.rqst_rdy <= 1
-    yield RisingEdge(dut.clk)
-    dut.rqst_rdy <= 0
-    # Now wait for the data to be sent
-    for i in range (300): yield RisingEdge(dut.clk)
-
-    # Error checking
+    # Error checking - CH1
     print "-------------------------------------"
-    print "FIFO_CH1_LEN=" + repr(len(src_ch1.fifo))
-    print "CH1 - FIFO_LEN=" + repr(len(ft245.fifo))
-    for i in range (len(ft245.fifo)):
-        a = ft245.fifo[i]
+    print "LEN_FIFO_CH1=" + repr(len(src_ch1.fifo))
+    print "LEN_FIFO_TX=" + repr(len(ft245.fifo))
+    print "TX\t||\tCH1"
+    for i in range (len(fifo_test_ch1)):
+        if(len(ft245.fifo) == 0):
+            a = "EMPTY"
+            err = 1
+        else:
+            a = ft245.fifo.pop(0)   #ft245.fifo[i]
         b = fifo_test_ch1[i]
         if (a == b):
-            print (repr(a) + ' == ' + repr(b) )
+            print (repr(a) + '\t==\t' + repr(b) )
         else:
-	        print (repr(a) + ' != ' + repr(b) )
+	        print (repr(a) + '\t!=\t' + repr(b) )
 	        err = 1
 
-    # Testing source: ch2
-    ft245.fifo = []
-    dut.rqst_id <= 2
-    dut.rqst_rdy <= 1
-    yield RisingEdge(dut.clk)
-    dut.rqst_rdy <= 0
-    # Now wait for the data to be sent
-    for i in range (300): yield RisingEdge(dut.clk)
-
-    # Error checking
+    # Error checking - CH2
     print "-------------------------------------"
-    print "FIFO_CH2_LEN=" + repr(len(src_ch2.fifo))
-    print "CH2 - tx length: " + repr(len(ft245.fifo))
-    for i in range (len(ft245.fifo)):
-        a = ft245.fifo[i]
+    print "LEN_FIFO_CH2=" + repr(len(src_ch2.fifo))
+    print "LEN_FIFO_TX: " + repr(len(ft245.fifo))
+    print "TX\t||\tCH2"
+    for i in range (len(fifo_test_ch2)):
+        if(len(ft245.fifo) == 0):
+            a = "EMPTY"
+            err = 1
+        else:
+            a = ft245.fifo.pop(0)   #ft245.fifo[i]
         b = fifo_test_ch2[i]
         if (a == b):
-            print (repr(a) + ' == ' + repr(b) )
+            print (repr(a) + '\t==\t' + repr(b) )
         else:
-	        print (repr(a) + ' != ' + repr(b) )
+	        print (repr(a) + '\t!=\t' + repr(b) )
 	        err = 1
 
     print "-------------------------------------"
--- modules/tx_protocol/tx_protocol.v
+++ modules/tx_protocol/tx_protocol.v
@@ -18,7 +18,7 @@
     Version:
                 Date            Modified by         Comment
                 2017/07/31      IP                  Module created. Only IO signals writen. Behavioural test hasn't been done.
-                2017/08/05      AK                  Completing module, adding testbench.
+                2017/08/05      AK                  Completing module. Testbench OK!!
     ToDo:
                 Date            Suggested by        Comment
                 2017/08/02      IP                  Package all input interfaces in a flattened 2D array ??
@@ -43,20 +43,20 @@ module tx_protocol #(
     // SI - Channel 1
     input [DATA_WIDTH-1:0] ch1_data,    // Data bus
     input ch1_rdy,                      // Valid data in the bus
-    output ch1_ack,                     // Ack
     input ch1_eof,                      // No more channel 1 data avalaible
+    output reg ch1_ack,                 // Ack
 
     // SI - Channel 2
     input [DATA_WIDTH-1:0] ch2_data,    // Data bus
     input ch2_rdy,                      // Valid data in the bus
-    output ch2_ack,                     // Ack
     input ch2_eof,                      // No more channel 1 data avalaible
+    output reg ch2_ack,                 // Ack
 
     // SI - Trigger status
-    input [DATA_WIDTH-1:0] tr_data,     // Data bus
-    input tr_rdy,                       // Valid data in the bus
-    output tr_ack,                      // Ack
-    input tr_eof                        // No more channel 1 data avalaible
+    input [DATA_WIDTH-1:0] trig_data,     // Data bus
+    input trig_rdy,                       // Valid data in the bus
+    input trig_eof,                       // No more channel 1 data avalaible
+    output reg trig_ack                   // Ack
 );
 
     reg [$clog2(SOURCES)-1:0] i = 0;
@@ -64,43 +64,89 @@ module tx_protocol #(
     localparam  ST_IDLE = 0,
                 ST_SENDING = 1;
 
-    reg state = 0;
+    reg state = ST_IDLE;
 
     reg [1:0] source_interface;
 
     // 2D array packing input interfaces.
-    wire [DATA_WIDTH-1:0] input_data [0:2];
-    wire [0:2] input_rdy;
-    wire [0:2] input_ack;
-    wire [0:2] input_eof;
+    /*wire [DATA_WIDTH-1:0] input_data [0:2];
+    wire input_rdy[0:2];
+    wire input_ack[0:2];
+    wire input_eof[0:2];
 
-    assign input_data[0] = tr_data;
+    assign input_data[0] = trig_data;
     assign input_data[1] = ch1_data;
     assign input_data[2] = ch2_data;
 
-    assign input_rdy[0] = tr_rdy;
+    assign input_rdy[0] = trig_rdy;
     assign input_rdy[1] = ch1_rdy;
     assign input_rdy[2] = ch2_rdy;
 
-    assign tr_ack = input_ack[0];
+    assign trig_ack = input_ack[0];
     assign ch1_ack = input_ack[1];
     assign ch2_ack = input_ack[2];
 
-    assign input_eof[0] = tr_eof;
+    assign input_eof[0] = trig_eof;
     assign input_eof[1] = ch1_eof;
     assign input_eof[2] = ch2_eof;
 
-    // Default value & MUXES
+    assign tx_data = (state == ST_SENDING) ? input_data[source_interface] : 0;
+    assign tx_rdy = (state == ST_SENDING) ? input_rdy[source_interface] : 0;
+
+    genvar h;
+    generate
+        for (h = 0; h < 3 ; h = h + 1)
+            assign input_ack[h] = (state == ST_SENDING && h == source_interface) ? tx_ack : 1'b0;
+    endgenerate
+    */
+
+    reg [DATA_WIDTH-1:0] input_data [0:SOURCES-1];
+    reg [0:SOURCES-1]input_rdy;
+    reg [0:SOURCES-1]input_eof;
+    reg [0:SOURCES-1]input_ack;
+
     always @* begin
-        tx_data = 0;
-        tx_rdy = 0;
-        for (i = 0; i < SOURCES; i = i +1) input_ack[i] = 1'b0;
+        input_data[0] <= trig_data;
+        input_data[1] <= ch1_data;
+        input_data[2] <= ch2_data;
+
+        input_rdy[0] <= trig_rdy;
+        input_rdy[1] <= ch1_rdy;
+        input_rdy[2] <= ch2_rdy;
+
+        input_eof[0] <= trig_eof;
+        input_eof[1] <= ch1_eof;
+        input_eof[2] <= ch2_eof;
+
+        trig_ack <= input_ack[0];
+        ch1_ack <= input_ack[1];
+        ch2_ack <= input_ack[2];
+
+        tx_data <= (state == ST_SENDING && input_eof[source_interface] == 1'b0) ? input_data[source_interface] : 0;
+        tx_rdy <= (state == ST_SENDING && input_eof[source_interface] == 1'b0) ? input_rdy[source_interface] : 0;
+    end
+
+    genvar h;
+    generate
+        for (h = 0; h < SOURCES ; h = h + 1)
+            always @* begin
+                input_ack[h] = (state == ST_SENDING && h == source_interface) ? tx_ack : 1'b0;
+            end
+    endgenerate
+
+
+    /*
+    always @* begin
+        tx_data <= 0;
+        tx_rdy <= 0;
+        for (i = 0; i < SOURCES; i = i +1) input_ack[i] <= 1'b0;
         if(state == ST_SENDING) begin
-            tx_data = input_data[source_interface];
-            tx_rdy = input_rdy[source_interface];
-            input_ack[source_interface] = tx_ack;
+            tx_data <= input_data[source_interface];
+            tx_rdy <= input_rdy[source_interface];
+            input_ack[source_interface] <= tx_ack;
         end
     end
+    */
 
     // State machine
     always @( posedge(clk) ) begin
@@ -112,10 +158,8 @@ module tx_protocol #(
                 state <= ST_SENDING;
                 for (i = 0; i<SOURCES ; i=i+1) if(input_rdy[i] == 1'b1) source_interface <= i;
             end
-            if( ( state == ST_SENDING ) ) begin
-                if( input_eof[source_interface] == 1'b1 ) begin
-                    state <= ST_IDLE;
-                end
+            if( state == ST_SENDING && input_eof[source_interface] == 1'b1 ) begin
+                state <= ST_IDLE;
             end
         end
     end
