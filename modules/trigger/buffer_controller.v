698feb79e2aafc4fc04093b444f06f5f6da683ce
Ariel <kuku2206@gmail.com>
2017-08-06 23:16:34 -0300

    added interface with tx_protocol for buffer_controller (trigger_status)

--- modules/trigger/buffer_controller.v
+++ modules/trigger/buffer_controller.v
@@ -59,14 +59,19 @@ module buffer_controller  #(
 
     // Requests
     input start,                // start signal inits cycle
+    input stop,                 // unused so far!!
+    input rqst_trigger_status,
 
     // Communication with RAM Controller
     output write_enable,
 
     // Communication with tx_control
-    input rqst_trigger_status,              //
-    output reg [7:0] trigger_status = 8'd0, // output frame [7:0] = {x,x,x,x,x,x,triggered_o,buffer_full_o}
-    output reg EOF = 1'b0                   // end of frame
+                  //
+
+    output reg [7:0] trigger_status_data = 8'd0, // output frame [7:0] = {x,x,x,x,x,x,triggered_o,buffer_full_o}
+    output reg trigger_status_rdy = 1'b0,
+    output reg trigger_status_eof = 1'b0,        // end of frame
+    input trigger_status_ack
 );
 
     localparam  ST_IDLE = 0,
@@ -108,70 +113,100 @@ module buffer_controller  #(
         end else begin
             if(input_rdy == 1'b1) counter <= counter + 1;
 
-            case (state)
+            if (start == 1'b1) begin
+                counter <= 0;
+                buffer_full_o <= 1'b0;
+                triggered_o <= 1'b0;
+                state <= ST_PRE_LOADING;
+            end else if(stop == 1'b1) begin
+                state <= ST_IDLE;
+            end else begin
+                case (state)
 
-                ST_IDLE:
-                begin
-                    if (start == 1'b1) begin
+                    ST_IDLE:
+                    begin
+                        // yep, nothing here.
+                        // Start changes state
+                    end
+
+                    // State to make sure that there are enough
+                    //  samples in the buffer before triggering
+                    ST_PRE_LOADING:
+                    begin
+                        if(counter == pre_trigger) state <= ST_WAITING_TRIGGER;
+                    end
+
+                    // State to search for trigger condition
+                    ST_WAITING_TRIGGER:
+                    begin
+                        if( counter == num_samples ) buffer_full_o <= 1'b1;
+                        if( triggered == 1'b1 ) begin
+                            counter <= pre_trigger;     // force to count from "pre_trigger" samples.
+                            buffer_full_o <= 1'b0;    // When triggers, resets buffer_full_o to wait for post-trigger samples
+                            triggered_o <= 1'b1;
+                            state <= ST_POST_LOADING;
+                        end
+                    end
+
+                    ST_POST_LOADING:
+                    begin
+                        if(counter == num_samples) begin
+                            buffer_full_o <= 1'b1;
+                            state <= ST_IDLE;
+                        end
+                    end
+
+                    default:
+                    begin
                         counter <= 0;
                         buffer_full_o <= 1'b0;
                         triggered_o <= 1'b0;
-                        state <= ST_PRE_LOADING;
-                    end
-                end
-
-                // State to make sure that there are enough
-                //  samples in the buffer before triggering
-                ST_PRE_LOADING:
-                begin
-                    if(counter == pre_trigger) state <= ST_WAITING_TRIGGER;
-                end
-
-                // State to search for trigger condition
-                ST_WAITING_TRIGGER:
-                begin
-                    if( counter == num_samples ) buffer_full_o <= 1'b1;
-                    if( triggered == 1'b1 ) begin
-                        counter <= pre_trigger;     // force to count from "pre_trigger" samples.
-                        buffer_full_o <= 1'b0;    // When triggers, resets buffer_full_o to wait for post-trigger samples
-                        triggered_o <= 1'b1;
-                        state <= ST_POST_LOADING;
-                    end
-                end
-
-                ST_POST_LOADING:
-                begin
-                    if(counter == num_samples) begin
-                        buffer_full_o <= 1'b1;
                         state <= ST_IDLE;
                     end
+                endcase
+            end
+        end
+    end
+
+    // Tx Protocol
+    localparam  ST2_IDLE=0,
+                ST2_SENDING=1;
+    reg state_2 = ST2_IDLE;
+    assign trigger_status_eof = (trigger_status_ack == 1'b1 || trigger_status_rdy == 1'b0) ? 1'b1 : 1'b0;
+    // comm with tx_control
+    always @(posedge clk) begin
+        trigger_status_data[0] <= buffer_full_o;
+        trigger_status_data[1] <= triggered_o;
+        if(rst == 1'b1) begin
+            trigger_status_rdy <= 1'b0;
+        end else begin
+            case(state_2)
+
+                ST2_IDLE:
+                begin
+                    if(rqst_trigger_status == 1'b1) begin
+                        trigger_status_rdy <= 1'b1;
+                        state_2 <= ST2_SENDING;
+                    end
                 end
 
-                default:
+                ST_SENDING:
                 begin
-                    counter <= 0;
-                    buffer_full_o <= 1'b0;
-                    triggered_o <= 1'b0;
-                    state <= ST_IDLE;
+                    if(trigger_status_ack == 1'b1) begin
+                        trigger_status_rdy <= 1'b0;
+                        state_2 <= ST2_IDLE;
+                    end
                 end
             endcase
         end
     end
 
-    // comm with tx_control
-    always @(posedge clk) begin
-        trigger_status[0] <= buffer_full_o;
-        trigger_status[1] <= triggered_o;
-        EOF <= 1'b0;
-        if(rqst_trigger_status == 1'b1) EOF <= 1'b1;
+    `ifdef COCOTB_SIM
+    initial begin
+      $dumpfile ("waveform.vcd");
+      $dumpvars (0,buffer_controller);
+      #1;
     end
-
-`ifdef COCOTB_SIM
-initial begin
-  $dumpfile ("waveform.vcd");
-  $dumpvars (0,buffer_controller);
-  #1;
-end
-`endif
+    `endif
 
 endmodule
