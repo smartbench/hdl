f612489f5da260522c156bb58adef1a8ad992c80
akukulanski <kuku2206@gmail.com>
2017-08-17 14:18:13 -0300

    changed defines

--- modules/adc_block/adc_block.v
+++ modules/adc_block/adc_block.v
@@ -34,17 +34,19 @@
 
 `timescale 1ns/1ps
 
+`include "conf_regs_defines.v"
+
 module adc_block #(
     parameter BITS_ADC = 8,//`__BITS_ADC,
     parameter ADC_DF_WIDTH   = 32,      // ADC decimation
     parameter MA_ACUM_WIDTH = 12,       // Moving Average Acumulator
     parameter REG_DATA_WIDTH = 16,//`REG_DATA_WIDTH,    // Simgle Interface
     parameter REG_ADDR_WIDTH = 8,//`REG_ADDR_WIDTH,
-    parameter ADC_DF_DV_REG = 0,
-    parameter MA_K_FACTOR_DV_REG = 3,
-    parameter REG_ADDR_ADC_DF_L = 0,
-    parameter REG_ADDR_ADC_DF_H = 1,
-    parameter REG_ADDR_MOV_AVE_K = 2
+    parameter DEFAULT_ADC_CLK_DIV = 0,
+    parameter DEFAULT_N_MOVING_AVERAGE = 3,
+    parameter ADDR_ADC_CLK_DIV_L = 0,
+    parameter ADDR_ADC_CLK_DIV_H = 1,
+    parameter ADDR_N_MOVING_AVERAGE = 2
   )(
     // Basic
     input clk_i,
@@ -68,9 +70,9 @@ module adc_block #(
     wire [BITS_ADC-1:0] adc_interface_si_data;
     wire adc_interface_si_rdy;
     wire adc_interface_si_ack;
-    reg [ADC_DF_WIDTH-1:0] adc_df                       = ADC_DF_DV_REG;  // adc decimation factor DefaultValue  register
-    reg [$clog2(MA_ACUM_WIDTH-BITS_ADC)-1:0] ma_k = MA_K_FACTOR_DV_REG; // moving average k factor DefaultValue register
-    
+    reg [ADC_DF_WIDTH-1:0] adc_df  = DEFAULT_ADC_CLK_DIV;  // adc decimation factor DefaultValue  register
+    reg [$clog2(MA_ACUM_WIDTH-BITS_ADC)-1:0] ma_k = DEFAULT_N_MOVING_AVERAGE; // moving average k factor DefaultValue register
+
     reg rst_restart = 1'b0;
     assign adc_interface_si_ack = adc_interface_si_rdy;
 
@@ -102,13 +104,13 @@ module adc_block #(
       .rdy_out            (si_rdy_o)
       );
 
-    
+
     // Registers
     always @ ( posedge(clk_i) ) begin
       rst_restart <= 1'b0;
       if (rst == 1'b1) begin
-        adc_df <= ADC_DF_DV_REG;
-        ma_k <= MA_K_FACTOR_DV_REG;
+        adc_df <= DEFAULT_ADC_CLK_DIV;
+        ma_k <= DEFAULT_N_MOVING_AVERAGE;
         rst_restart <= 1'b1;
       end else begin
         if (reg_si_rdy==1'b1) begin
@@ -117,27 +119,27 @@ module adc_block #(
             // NOTE: not generic Description!! If ADC_DF_WIDTH or REG_DATA_WIDTH changes
             // gotta check the lines below
             // NOTE ANSWER: added a 1 clock duration reset for each time something changes
-            REG_ADDR_ADC_DF_L:
+            ADDR_ADC_CLK_DIV_L:
             begin
               adc_df[REG_DATA_WIDTH-1:0] <= reg_si_data;
               rst_restart <= 1'b1;
             end
-            REG_ADDR_ADC_DF_H:
+            ADDR_ADC_CLK_DIV_H:
             begin
               adc_df[ADC_DF_WIDTH-1:REG_DATA_WIDTH] <= reg_si_data;
               rst_restart <= 1'b1;
             end
-              
-            REG_ADDR_MOV_AVE_K:
+
+            ADDR_N_MOVING_AVERAGE:
             begin
               ma_k <= reg_si_data[$clog2(MA_ACUM_WIDTH-BITS_ADC)-1:0];
               rst_restart <= 1'b1;
              end
-            
+
             default:
             begin
             end
-            
+
           endcase
         end
       end
